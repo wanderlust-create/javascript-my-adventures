1eb74fbfb2d751029745c5d8c287cdb5a4fcd795
Tamara Dowis <tamara.dowis@gmail.com>
2022-12-28 10:42:36 -0600

    add swagger doc functionality 

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const setupDb = require("./utils/db-setup");
 
+// load winston logger
+const logger = require("./utils/logger");
+
 // Routes
 const api = require("./api");
 
 // .env variables
 const config = require("./config");
-const logger = require("./utils/logger");
+
+// swagger docs
+const swaggerDocs = require("./utils/swagger");
 
 // set up database with objection and knex
 setupDb();
@@ -15,13 +20,15 @@ const app = express();
 app.use(express.json());
 app.use(api);
 
+
 app.listen(config.PORT, (err) => {
   if (err) {
     logger.error(err);
     process.exit(1);
     return;
   }
+  swaggerDocs(app, config.PORT);
   logger.info(`
-      %%%% 👍  API REST Server listening on http://localhost:${config.PORT} 👍 %%%%
+      %%%% 👍  My Adventure JS Swagger API REST Server listening on http://localhost:${config.PORT} 👍 %%%%
     `);
 });
